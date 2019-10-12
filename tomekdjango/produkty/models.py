3ef1b6ec67e024a5e9cde2bd74c9cfe0861a23c4
Tomasz Madziara <t.madziara@gmail.com>
2019-10-12 23:51:22 +0200

    Nowe typy pól dla modeli

--- tomekdjango/produkty/models.py
+++ tomekdjango/produkty/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Produkt(models.Model):
 	nazwa = models.TextField()
-	opis = models.TextField()
-	cena = models.TextField()
+	opis = models.TextField(blank=True, null=True)
+	cena = models.DecimalField(max_digits=10000, decimal_places=2)
 	podsumowanie = models.TextField(default = 'Django to najlepszy framework')
