7cb839ff9d024f3c99e52e74117bdbb14da4d991
Tomasz Madziara <t.madziara@gmail.com>
2019-10-15 22:38:34 +0200

    Template Context

--- tomekdjango/strony/views.py
+++ tomekdjango/strony/views.py
@@ -9,6 +9,11 @@ def home_widok(request, *args, **kwargs):
 def kontakt(request, *args, **kwargs):
 	return render (request, "kontakt.html", {})
 def oNas(request, *args, **kwargs):
-	return render (request, "onas.html", {})
+	kontekst = {
+		"data":"otwarcie sklepu: maj 1997",
+		"liczba":"szacunkowa ilość klientów na dzień: 135",
+		"kody" : [12,14,16,17,18]
+	}
+	return render (request, "onas.html", kontekst)
 def media(request, *args, **kwargs):
 	return HttpResponse("<h1> Facebook, Instagram, Twitter </h1>") #String html
