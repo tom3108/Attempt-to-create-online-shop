00e3acedff510c4a3a03c59aed445fb3cf68cc9b
Tomasz Madziara <t.madziara@gmail.com>
2019-10-15 21:12:26 +0200

    HTML templates. Inheritance

--- tomekdjango/strony/views.py
+++ tomekdjango/strony/views.py
@@ -7,8 +7,8 @@ def home_widok(request, *args, **kwargs):
 	print(request.user)
 	return render (request, "dom.html", {})
 def kontakt(request, *args, **kwargs):
-	return HttpResponse("<h1> Kontakt mailowy: t.madziara@gmail.com </h1>") #String html
+	return render (request, "kontakt.html", {})
 def oNas(request, *args, **kwargs):
-	return HttpResponse("<h1> Sklep z 20 letnią historią </h1>") #String html
+	return render (request, "onas.html", {})
 def media(request, *args, **kwargs):
 	return HttpResponse("<h1> Facebook, Instagram, Twitter </h1>") #String html
